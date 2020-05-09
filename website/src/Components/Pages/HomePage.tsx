e8778206e5d5049baeec7717632501606fddf088
Andrew Dimmer <contactandrewd@gmail.com>
2020-05-09 13:33:44 -0400

    feat(website): Added Navigation System and Home Page

---
+++ website/src/Components/Pages/HomePage.tsx
@@ -0,0 +1,45 @@
+import { Container, Typography, Button } from "@material-ui/core";
+import React, { Fragment } from "react";
+import { styles } from "../../Styles";
+import { PageProps } from ".";
+
+const HomePage: React.FunctionComponent<PageProps> = ({
+  currentUser,
+  setPageKey,
+}) => {
+  const classes = styles();
+
+  return (
+    <Fragment>
+      <Container className={classes.pageTitle}>
+        <Typography variant="h3">Dance Til You Drop!</Typography>
+      </Container>
+      <Button
+        color="primary"
+        fullWidth
+        variant="contained"
+        size="large"
+        className={classes.marginedTopBottom}
+        onClick={() => {
+          setPageKey("play");
+        }}
+      >
+        <Typography variant="h4">Play!</Typography>
+      </Button>
+      <Button
+        color="primary"
+        fullWidth
+        variant="outlined"
+        size="large"
+        className={classes.marginedTopBottom}
+        onClick={() => {
+          currentUser ? setPageKey("logout") : setPageKey("login");
+        }}
+      >
+        <Typography variant="h4">{currentUser ? "Logout" : "Login"}</Typography>
+      </Button>
+    </Fragment>
+  );
+};
+
+export default HomePage;
---
+++ website/src/Components/Pages/index.ts
@@ -0,0 +1,106 @@
+import { SvgIconTypeMap } from "@material-ui/core";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import HomeIcon from "@material-ui/icons/Home";
+import { UserProfile } from "../../Scripts/firebaseUserTypes";
+import { NotificationMessage } from "../Misc/Notifications";
+import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
+import LogoutPage from "./LogoutPage";
+import ProfilePage from "./ProfilePage";
+import HomePage from "./HomePage";
+
+export declare interface PageProps {
+  setPageKey: (pageKey: string) => void;
+  setLoadingMessage: (loadingMessage: string) => void;
+  setNotification: (notification: NotificationMessage) => void;
+  handleLoadUserData: (
+    userId: string,
+    displayName?: string,
+    email?: string,
+    photoUrl?: string
+  ) => void;
+  currentUser: firebase.User | null;
+  currentUserProfile: UserProfile | null;
+  classes: any;
+}
+
+export declare interface PageListForMenuItem {
+  key: string;
+  menuLabel: string;
+  menuIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  displaySignedIn: boolean;
+  displaySignedOut: boolean;
+}
+
+declare interface PageListItem extends PageListForMenuItem {
+  title: string;
+  component: React.FunctionComponent<PageProps>;
+}
+
+const pageList: PageListItem[] = [
+  {
+    key: "home",
+    title: "",
+    menuLabel: "Home",
+    menuIcon: HomeIcon,
+    displaySignedIn: true,
+    displaySignedOut: true,
+    component: HomePage,
+  },
+  {
+    key: "login",
+    title: "",
+    menuLabel: "Join or Login",
+    menuIcon: ExitToAppIcon,
+    displaySignedIn: false,
+    displaySignedOut: true,
+    component: LoginPage,
+  },
+  {
+    key: "profile",
+    title: "Profile",
+    menuLabel: "Profile",
+    menuIcon: AccountCircleIcon,
+    displaySignedIn: true,
+    displaySignedOut: false,
+    component: ProfilePage,
+  },
+  {
+    key: "logout",
+    title: "",
+    menuLabel: "Logout",
+    menuIcon: ExitToAppIcon,
+    displaySignedIn: true,
+    displaySignedOut: false,
+    component: LogoutPage,
+  },
+];
+
+const pageObject = pageList.reduce((pageObject, pageListItem) => {
+  pageObject[pageListItem.key] = pageListItem;
+  return pageObject;
+}, {} as { [key: string]: PageListItem });
+
+export const getPageComponent = (
+  key: string
+): React.FunctionComponent<PageProps> => {
+  if (pageObject[key]) {
+    return pageObject[key].component;
+  }
+  return ErrorPage;
+};
+
+export const getPageTitle = (key: string): string => {
+  if (pageObject[key]) {
+    return pageObject[key].title;
+  }
+  return "";
+};
+
+export const pageListForMenu: PageListForMenuItem[] = pageList.map(
+  ({ key, menuLabel, menuIcon, displaySignedIn, displaySignedOut }) => {
+    return { key, menuLabel, menuIcon, displaySignedIn, displaySignedOut };
+  }
+);
